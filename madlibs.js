e17e842156b51bbfa83ce97e6bbb21f134f3e88e
Yonas CS <75498605+JonahCS@users.noreply.github.com>
2022-06-01 16:24:19 +0300

    Create madlibs.js

---
+++ madlibs.js
@@ -0,0 +1,74 @@
+/* mad libs coding project for "five live free code school" */
+
+
+//Title of the mad libs game 
+console.log("MAD LIBS : A Camping We Will Go! ")
+
+//Getting user input from the console
+let input1 = prompt("Enter a Noun(a person's name)")=;
+let input2 = prompt("Enter a Noun");
+let input3 = prompt("Enter an Adjective");
+let input4 = prompt("Enter a Verb");
+let input5 = prompt("Enter and Adjective");
+let input6 = prompt("Enter Animal");
+let input7 = prompt("Enter a Verb");
+let input8 = prompt("Enter a Color");
+let input9 = prompt("Enter a Verb");
+let input10 = prompt("Enter an Adverb(ending in ly)");
+let input11 = prompt("Enter a Number");
+let input12 = prompt("Enter Measure of Time");
+let input13 = prompt("Enter Color");
+let input14 = prompt("Enter Animal");
+let input15 = prompt("Enter Number");
+let input16 = prompt("Enter a Silly Word");
+let input17 = prompt("Enter a Noun");
+
+//printing out a mad lib to the console
+console.log(`This weekend I am going camping with ${input1}. 
+I packed my lantern, sleeping bag, and ${input2}. I am so ${input3} to
+${input4} in a tent. I am ${input5} we
+might see a ${input6}, they are kind of dangerous.
+We are going to hike, fish, and ${input7}.
+I have heard that the ${input8} lake is great for ${input9}. Then we will
+${input10} hike through the forest for ${input11} ${input12}. If I see a
+${input13} ${input14} while hiking, I am going to bring it home as a pet! At night we will tell
+${input15} ${input16} stories and roast ${input17} around the campfire!! `);
+
+
+/* ******************************************************************************************* */
+
+//Title of the mad libs game 
+console.log("MAD LIBS : Enchanted Forest");
+
+//Getting user input form the console
+let input1 = prompt("Enter Person's Name");
+let input2 = prompt("Enter Adjective");
+let input3 = prompt("Enter Color");
+let input4 = prompt("Enter Animal");
+let input5 = prompt("Enter Place");
+let input6 = prompt("Enter Adjective");
+let input7 = prompt("Enter Magical Creature(plural)")
+let input8 = prompt("Enter Adjective");
+let input9 = prompt("Enter Magical Creature(plural)")
+let input10 = prompt("Enter Room in the House");
+let input11 = prompt("Enter Noun");
+let input12 = prompt("Enter Noun");
+let input13 = prompt("Enter Noun");
+let input14 = prompt("Enter Adjective");
+let input15 = prompt("Enter Noun");
+let input16 = prompt("Enter Number");
+let input17 = prompt("Enter Measure of Time");
+let input18 = prompt("Enter Verb(ending in ing)");
+let input19 = prompt("Enter Adjective");
+let input20 = prompt("Enter Noun");
+
+//printing out a mad lib to the console
+
+console.log(`Dear ${input1},
+I am writing to you from a ${input2} castle in an enchanted forest. I found myself here one day after
+going for a ride on a ${input3} ${input4} in ${input5}. There are
+${input6} ${input7} and ${input8} ${input9} here! In the
+${input10} there is a pool full of ${input11}. I fall asleep each night on a ${input12} of
+${input13} and dream of ${input14} ${input15}. It feels as though I have lived here for
+${input16} ${input17}. I hope one day you can visit, although the only way to get here now is
+${input18} on a ${input19} ${input10}!! `);
